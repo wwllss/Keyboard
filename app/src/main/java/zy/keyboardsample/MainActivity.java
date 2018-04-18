922b180ee95b8366c323d5021fd272b925b1fe05
zhangyuan <zhangyuan@kesucorp.com>
2018-04-18 10:10:13 +0800

    sample

--- app/src/main/java/zy/keyboardsample/MainActivity.java
+++ app/src/main/java/zy/keyboardsample/MainActivity.java
@@ -2,6 +2,14 @@ package zy.keyboardsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import zy.keyboard.InputAction;
+import zy.keyboard.KeyboardUtil;
+import zy.keyboard.NumberKeyboard;
+import zy.keyboard.OnInputListener;
+import zy.keyboard.OnKeyCodeListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = findViewById(R.id.edit_text);
+
+        KeyboardUtil.hideSoftInputMethod(editText);
+
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showKeyboard(editText);
+            }
+        });
+
+    }
+
+    private void showKeyboard(final EditText editText) {
+
+        final NumberKeyboard keyboard = new NumberKeyboard(this);
+        final OnInputListener onInputListener = new OnInputListener() {
+            @Override
+            public void onInputChanged(String text) {
+                editText.setText(text);
+            }
+
+            @Override
+            public void onInputFinished(String text) {
+                editText.setText(text);
+            }
+        };
+        keyboard.setOnInputListener(onInputListener);
+        keyboard.setOnKeyCodeListener(new OnKeyCodeListener() {
+            @Override
+            public boolean onKeyDown(int keyCode) {
+                if (InputAction.ENTER == keyCode
+                        || InputAction.KEYBOARD_HIDE == keyCode) {
+                    keyboard.detach();
+                    onInputListener.onInputFinished(keyboard.getText());
+                    return true;
+                }
+                return false;
+            }
+        });
+        keyboard.attach(this, editText);
     }
 }
