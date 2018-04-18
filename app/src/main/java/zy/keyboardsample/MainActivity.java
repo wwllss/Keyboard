e8fc6a0e34e52f49eb08ad7f46a86fee3c2fc764
zhangyuan <zhangyuan@kesucorp.com>
2018-04-18 10:17:32 +0800

    selection

--- app/src/main/java/zy/keyboardsample/MainActivity.java
+++ app/src/main/java/zy/keyboardsample/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onInputFinished(String text) {
                 editText.setText(text);
+                editText.setSelection(text.length());
             }
         };
         keyboard.setOnInputListener(onInputListener);
