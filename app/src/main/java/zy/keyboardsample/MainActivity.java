7c6f5142228f8038f266604cb2432b3524271453
zhangyuan <fromngatoo@gmail.com>
2018-06-22 22:54:33 +0800

    sample

--- app/src/main/java/zy/keyboardsample/MainActivity.java
+++ app/src/main/java/zy/keyboardsample/MainActivity.java
@@ -3,9 +3,11 @@ package zy.keyboardsample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import zy.keyboard.InputAction;
+import zy.keyboard.KeyboardLayout;
 import zy.keyboard.KeyboardUtil;
 import zy.keyboard.NumberKeyboard;
 import zy.keyboard.OnInputListener;
@@ -22,18 +24,26 @@ public class MainActivity extends AppCompatActivity {
 
         KeyboardUtil.hideSoftInputMethod(editText);
 
+        final CheckBox checkBox = findViewById(R.id.checkbox);
+
         editText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showKeyboard(editText);
+                showKeyboard(editText, checkBox.isChecked());
             }
         });
 
     }
 
-    private void showKeyboard(final EditText editText) {
+    private void showKeyboard(final EditText editText, boolean checked) {
 
-        final NumberKeyboard keyboard = new NumberKeyboard(this);
+        final KeyboardLayout keyboard;
+        if (checked) {
+            keyboard = new KeyboardLayout(this);
+            keyboard.setKeyboardView(R.layout.layout_keyboard_number);
+        } else {
+            keyboard = new NumberKeyboard(this);
+        }
         final OnInputListener onInputListener = new OnInputListener() {
             @Override
             public void onInputChanged(String text) {
